0789fc6e4ba2fb255ba48ccdce59b583581cde75
yodho1psnrls <todkit@abv.bg>
2025-02-04 11:40:29 +0200

    Palindromes

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,24 @@
+def is_text_char(ch: str) -> bool:
+    ch = ch.lower()
+    return ord("a") <= ord(ch) <= ord("z")
+
+
+def only_alphabetic(word: str) -> str:
+    result = ""
+    for ch in word:
+        if is_text_char(ch):
+            result += ch
+    return result
+
+
 def is_palindrome(word: str) -> bool:
+    # Leave only alphabetic characters
+    word = only_alphabetic(word)
+    # Remove spaces
+    word = word.replace(" ", "")
+    # Make all characters lower case
+    word = word.lower()
+
     n = len(word)
     for i in range(n // 2):
         if word[i] != word[n - i - 1]:
@@ -11,4 +31,8 @@ if __name__ == "__main__":
 
     while True:
         word = str(input())
+        # print(is_palindrome(word))
         print(is_palindrome(word))
+
+
+#
